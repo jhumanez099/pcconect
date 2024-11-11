dc9ea6db5005e9061e857e84d9cb3852bb9f0a8c
Santiortiz <santiortiz2225@gmail.com>
2024-11-11 17:30:02 -0500

    Se hizo tipo usuario

--- Front/Cliente/src/App.jsx
+++ Front/Cliente/src/App.jsx
@@ -1,13 +1,14 @@
+import TipoUsuario from '../src/Pages/Admin/tipoUsuario/tipoUsuario'
 import './App.css'
 import Clientes from './Pages/Admin/Clientes/Clientes'
+import Usuarios from './Pages/Admin/Usuarios/Usuarios'
 
 
 export default function App() {
 
   return (
     
-    <Clientes> </Clientes>
-  
+    <Usuarios></Usuarios>
   )
 }
 
---
+++ Front/Cliente/src/Pages/Admin/Usuarios/Usuarios.jsx
@@ -0,0 +1,146 @@
+import Axios from "axios";
+import Navbar from "../../../components/Navbar";
+import { useForm } from "react-hook-form";
+import { useState } from "react";
+
+export default function Usuarios() {
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        if (Object.keys(data).length === 0) {
+            return console.log("Campos vacíos");
+        }
+
+        crearUsuario(data);
+    };
+
+    const crearUsuario = (usuarioData) => {
+        Axios.post("http://localhost:3000/api/usuarios", {
+            idTipoUsuario: usuarioData.idTipoUsuario,
+            nombreUsuario: usuarioData.nombreUsuario,
+            contraseñaUsuario: usuarioData.contraseñaUsuario,
+            telefonoUsuario: usuarioData.telefonoUsuario,
+            correoUsuario: usuarioData.correoUsuario,
+            cargoUsuario: usuarioData.cargoUsuario,
+            estadoUsuario: usuarioData.estadoUsuario
+
+        })
+            .then((response) => {
+                console.log("Usuario creado", response.status);
+            })
+            .catch((error) => {
+                console.log("Error al crear el usuario:", error);
+            });
+    };
+
+    return (
+        <div className="vh-100 d-flex justify-content-center align-items-center" style={{ backgroundColor: "#0a6e76" }}>
+            <div className="row p-5">
+                <Navbar />
+            </div>
+            <div className="rounded card shadow p-5" style={{ width: "650px", height: "auto" }}>
+                <div className="row my-5 gx-5">
+                    <div className="col-4 col-md-4">
+                        <a href="#" className="text-primary">← Back</a>
+                    </div>
+                    <div className="col-md-6 col-sm-9">
+                        <h1 className="text-center">Crear usuario</h1>
+                    </div>
+                </div>
+
+                <div className="row justify-content-center">
+                    <form onSubmit={handleSubmit(onSubmit)}>
+                    <div className="mb-4 row align-items-center">
+                            <label htmlFor="idTipoUsuario" className="col-sm-3 col-form-label text-end">Tipo usuario:</label>
+                            <div className="col-sm-9">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="idTipoUsuario"
+                                    style={{ maxWidth: "250px" }}
+                                    {...register("idTipoUsuario")}
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 row align-items-center">
+                            <label htmlFor="NombreUsuario" className="col-sm-3 col-form-label text-end">Nombre:</label>
+                            <div className="col-sm-9">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="nombreUsuario"
+                                    style={{ maxWidth: "250px" }}
+                                    {...register("nombreUsuario")}
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 row align-items-center">
+                            <label htmlFor="ContraseñaUsuario" className="col-sm-3 col-form-label text-end">Contraseña:</label>
+                            <div className="col-sm-9">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="contraseñaUsuario"
+                                    style={{ maxWidth: "250px" }}
+                                    {...register("contraseñaUsuario")}
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 row align-items-center">
+                            <label htmlFor="TelefonoUsuario" className="col-sm-3 col-form-label text-end">Teléfono:</label>
+                            <div className="col-sm-9">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="telefonoUsuario"
+                                    style={{ maxWidth: "250px" }}
+                                    {...register("telefonoUsuario")}
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 row align-items-center">
+                            <label htmlFor="CorreoUsuario" className="col-sm-3 col-form-label text-end">Correo:</label>
+                            <div className="col-sm-9">
+                                <input
+                                    type="email"
+                                    className="form-control"
+                                    id="correoUsuario"
+                                    style={{ maxWidth: "250px" }}
+                                    {...register("correoUsuario")}
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 row align-items-center">
+                            <label htmlFor="CargoUsuario" className="col-sm-3 col-form-label text-end">Cargo:</label>
+                            <div className="col-sm-9">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="cargoUsuario"
+                                    style={{ maxWidth: "250px" }}
+                                    {...register("cargoUsuario")}
+                                />
+                            </div>
+                        </div>
+                        <div className="mb-4 row align-items-center">
+                            <label htmlFor="EstadoUsuario" className="col-sm-3 col-form-label text-end">Estado:</label>
+                            <div className="col-sm-9">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    id="estadoUsuario"
+                                    style={{ maxWidth: "250px" }}
+                                    {...register("estadoUsuario")}
+                                />
+                            </div>
+                        </div>
+                        <div className="d-grid gap-2 d-md-block text-center">
+                            <button type="submit" className="btn" style={{ backgroundColor: "#c8e6c9", color: "black" }}>Crear</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+    );
+}
